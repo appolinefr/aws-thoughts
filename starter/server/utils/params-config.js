714013211713e6b09d3815b5fdca6ac5dc1a8e91
Appoline Cogan <appolinecogan@gmail.com>
2022-12-23 11:33:33 +1100

    integrated  database calls and form data requests into the application.

---
+++ starter/server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require("uuid");
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split(".");
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: "user-images-198b125a-22ed-4ad6-95d2-2a64e221b9e2",
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
